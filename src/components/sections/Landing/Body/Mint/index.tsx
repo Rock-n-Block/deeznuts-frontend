9320a4eddde9fa4e7bf569d055482b101bb74b42
dnotrad <lentsd@bk.ru>
2021-09-24 16:37:29 +0300

    add public desgin (not end)

--- src/components/sections/Landing/Body/Mint/index.tsx
+++ src/components/sections/Landing/Body/Mint/index.tsx
@@ -1,4 +1,9 @@
-import { is_presale } from '../../../../../config/index';
+import { useCallback, useEffect, useState } from 'react';
+import { is_presale, backendUrl } from '../../../../../config/index';
+
+import { useWeb3Context } from '../../../../context/WalletConnect';
+import { useModals } from '../../../../context/Modal';
+import { notify } from '../../../../utils/notify';
 
 import s from './Mint.module.scss';
 
@@ -7,9 +12,153 @@ import nft2 from '../../../../../assets/img/sections/landing/mint/nft-2.png';
 import nft3 from '../../../../../assets/img/sections/landing/mint/nft-3.png';
 import nft4 from '../../../../../assets/img/sections/landing/mint/nft-4.png';
 
+import WalletModal from '../../../molecules/Modals/WalletModal/index';
+import MintModal, { IMintModalProps } from '../../../molecules/Modals/MintModal/index';
+// import { is_production, backendUrl } from '../../../../config/index';
+
+// import devil from '../../../../assets/img/sections/landing/first-block-presale/devil-nft.png';
+// import light from '../../../../assets/img/sections/landing/first-block-presale/lightning-nft.png';
+// import cosmo from '../../../../assets/img/sections/landing/first-block-presale/cosmo-nft.png';
+
+const TIME_FOR_UPDATE = 20000;
+
 const Mint: React.FC = () => {
+  const [lastTimerId, setLastTimerId] = useState<Array<NodeJS.Timeout>>([]);
+
+  const { init, sendEth } = useWeb3Context();
+  const { setModal } = useModals();
+
+  const [modalsData, setModalsData] = useState<Array<IMintModalProps>>([]);
+
+  const getInfoAboutTx = useCallback(
+    async (txHash: string) => {
+      const headers = await fetch(`${backendUrl}payments/${txHash}/`);
+      const data = await headers.json();
+
+      if (data.status === 'SUCCESS') {
+        const hashesFromLs = localStorage.getItem('txHashes');
+        const hashes = hashesFromLs ? await JSON.parse(hashesFromLs) : [];
+
+        if (hashes.includes(txHash)) {
+          setModalsData((prevState) => [
+            ...prevState.filter((modal) => modal.txHash !== txHash),
+            {
+              type: data.rarity === 'common' ? 'COMMON' : 'LEGENDARY',
+              img: data.image,
+              txHash,
+              id: data.id,
+            },
+          ]);
+
+          setModal(txHash);
+        }
+      }
+    },
+    [setModal],
+  );
+
+  const mintNft = async (wallet: 'MetaMask' | 'WalletConnect') => {
+    // if (!Object.values(timeBeforeEnd).every((el) => el === 0) && is_production) {
+    //   notify("The presale hasn't started yet", 'error');
+    //   return;
+    // }
+    const info = await init(wallet);
+
+    if (!info) {
+      notify('No Web3 Provider! Please install or download MetaMask', 'error');
+      return;
+    }
+
+    if (info.code === 3) {
+      notify(`${info.message.message} Connect your wallet!`, 'error');
+      return;
+    }
+
+    if ([404, 4].includes(info.code)) {
+      notify(info.message.text, 'error');
+      return;
+    }
+
+    if (info && !info.code) {
+      try {
+        const backendData = await fetch(`${backendUrl}info/?format=json`);
+        const data = await backendData.json();
+
+        if (data.minted >= data.total_mint_amount) {
+          notify('All nfts are minted!', 'error');
+        } else if (data.address) {
+          notify('Please wait for your transaction to be approved!');
+
+          const txRes = await sendEth({
+            from: info.address,
+            to: data.address,
+            value: data.amount,
+          });
+
+          if (txRes.status) {
+            const hashesFromLS = localStorage.getItem('txHashes');
+            const hashes = hashesFromLS ? JSON.parse(hashesFromLS) : [];
+            hashes.push(txRes.transactionHash);
+
+            localStorage.setItem('txHashes', JSON.stringify(hashes));
+
+            notify('The transaction has been sent!', 'success');
+            notify(
+              'Please stay on the site, your token will be generated within a couple of minutes!',
+              'success',
+            );
+
+            const timerId = setInterval(() => {
+              hashes.forEach((txHash: string) => {
+                getInfoAboutTx(txHash);
+              });
+            }, TIME_FOR_UPDATE);
+
+            // current timer id
+            setLastTimerId((prev) => [...prev, timerId]);
+          }
+        }
+      } catch (error: any) {
+        notify(error.message, 'error');
+      }
+    }
+  };
+
+  useEffect(() => {
+    const hashesFromLS = localStorage.getItem('txHashes');
+    const hashes = hashesFromLS ? JSON.parse(hashesFromLS) : [];
+    if (hashes.length > 0) {
+      hashes.forEach((txHash: string) => {
+        getInfoAboutTx(txHash);
+      });
+
+      setInterval(() => {
+        hashes.forEach((txHash: string) => {
+          getInfoAboutTx(txHash);
+        });
+      }, TIME_FOR_UPDATE);
+    }
+  }, [getInfoAboutTx]);
+
+  useEffect(() => {
+    if (lastTimerId.length >= 2) {
+      const prevId = lastTimerId[lastTimerId.length - 2];
+      clearInterval(prevId);
+    }
+  }, [lastTimerId]);
+
   return (
     <section className={s.block}>
+      <WalletModal mintNft={mintNft} />
+      {modalsData.map((data) => (
+        <MintModal
+          key={data.txHash}
+          txHash={data.txHash}
+          img={data.img}
+          type={data.type}
+          id={data.id}
+        />
+      ))}
       <div className={s.block_inner}>
         {!is_presale && (
           <div className={s.left}>
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,15 +1,13 @@
 import { ToastContainer } from 'react-toastify';
 
-import FirstBlock from '../../components/sections/Landing/FirstBlock/index';
 import FirstBlockPresale from '../../components/sections/Landing/FirstBlockPresale/index';
 import LandingBody from '../../components/sections/Landing/Body/index';
-import { is_presale } from '../../config/index';
 
 const Landing: React.FC = () => {
   return (
     <>
       <ToastContainer limit={5} />
-      {is_presale ? <FirstBlockPresale /> : <FirstBlock />}
+      <FirstBlockPresale />
       <LandingBody />
     </>
   );
