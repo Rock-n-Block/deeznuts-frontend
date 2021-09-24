24749efcd20432b03e11c56926388c7cf7fc4f7a
dnotrad <lentsd@bk.ru>
2021-09-24 12:58:48 +0300

    upd hand to loader

--- src/components/molecules/Modals/MintModal/index.tsx
+++ src/components/molecules/Modals/MintModal/index.tsx
@@ -1,12 +1,11 @@
 import ModalWrapper from '../Modal';
 import { useModals } from '../../../../context/Modal';
-// import { open_sea_link } from '../../../../config/index';
 import ImgWithPreload from '../../../atoms/ImgWithPreload/index';
 
 import s from './MintModal.module.scss';
 
 import discrod from '../../../../assets/img/icons/discord-blue.svg';
-import nftPlaceholder from '../../../../assets/img/nft-placeholder.png';
+import loader from '../../../../assets/img/icons/loader.svg';
 
 export interface IMintModalProps {
   type: 'COMMON' | 'LEGENDARY';
@@ -44,7 +43,7 @@ const MintModal: React.FC<IMintModalProps> = ({ type, img, txHash, id }) => {
             href={`https://testnets.opensea.io/assets/${contractId}/${id - 1}`}
             className={`${s.img} ${type === 'LEGENDARY' && s.legendary}`}
           >
-            <ImgWithPreload preloader={nftPlaceholder} img={img} />
+            <ImgWithPreload preloader={loader} img={img} />
             <div className={s.img_bg} />
           </a>
         </div>
